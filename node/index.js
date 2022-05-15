b82847ed9b020ebd9000cb419e4d4917033c53c8
Timmu <timukasr@gmail.com>
2022-05-15 22:59:34 +0300

    Re-use worker

--- node/index.js
+++ node/index.js
@@ -10,23 +10,42 @@ for (let i = 0; i < 50; i++) {
   circles.push({ x: Math.random() * 500, y: Math.random() * 500 });
 }
 
+const count = 10;
+
 async function runner(lkh, name) {
+  console.log("Starting", name);
   const start = Date.now();
-  const result = await lkh(circles);
+
+  for (let i = 0; i < count; i++) {
+    await lkh(circles);
+  }
+  // const result = await lkh(circles);
   const duration = Date.now() - start;
-  console.log(`Path for ${circles.length} nodes found in ${duration}ms using ${name}`, result);
+  console.log(`Path for ${circles.length} nodes X ${count} found in ${duration}ms using ${name}`);
 }
 
+const promises = new Map();
+
+const worker = new Worker(path.join(__dirname, "./lkh-node-worker.js"));
+
+worker.on("message", data => {
+  const resolve = promises.get(data.id);
+
+  if (resolve) {
+    resolve(data.path);
+    promises.delete(data.id);
+  }
+});
+worker.on("error", err => console.error(`Worker returned error ${err}`));
+worker.on("exit", code => {
+  console.error(`Worker stopped with exit code ${code}`);
+});
+
 function runService(circles) {
-  return new Promise((resolve, reject) => {
-    const worker = new Worker(path.join(__dirname, "./lkh-node-worker.js"), {
-      workerData: { circles, id: Math.random() },
-    });
-    worker.on("message", resolve);
-    worker.on("error", reject);
-    worker.on("exit", code => {
-      if (code !== 0) reject(new Error(`Worker stopped with exit code ${code}`));
-    });
+  return new Promise((resolve, _reject) => {
+    const id = Math.random();
+    promises.set(id, resolve);
+    worker.postMessage({ id, circles });
   });
 }
 
@@ -35,7 +54,7 @@ async function run() {
 
   await setTimeout(100);
 
-  await runner(runService, "blocking");
+  await runner(runService, "worker");
 
   await setTimeout(100);
 }
--- node/lkh-node-worker.js
+++ node/lkh-node-worker.js
@@ -1,11 +1,11 @@
-const { workerData, parentPort } = require("worker_threads");
+const { parentPort } = require("worker_threads");
 const lkh = require("./lkh");
 
-// You can do any heavy stuff here, in a synchronous way
-// without blocking the "main thread"
-lkh(workerData.circles).then(path => {
-  parentPort.postMessage({
-    id: workerData.id,
-    path,
+parentPort.on("message", msg => {
+  lkh(msg.circles).then(path => {
+    parentPort.postMessage({
+      id: msg.id,
+      path,
+    });
   });
 });
